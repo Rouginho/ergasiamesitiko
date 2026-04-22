e2371f0c9d74130a999c41f645a2b38bc31e021f
Rouginho <alexandroska2005@gmail.com>
2026-04-22 14:43:16 +0300

    Fix Supabase anon key

--- site/js/config.js
+++ site/js/config.js
@@ -5,4 +5,4 @@
 // ============================================================
 
 const SUPABASE_URL  = 'https://uhhzhdippdhixdagrjmm.supabase.co';
-const SUPABASE_KEY  = 'sb_publishable_p_TZqNGzGq9i-6yL1dNcFg_4qTGNU1z';
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoaHpoZGlwcGRoaXhkYWdyam1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NDc5MTYsImV4cCI6MjA5MjQyMzkxNn0.LBy51hhUUGexW7EbJBruFqizxm38QN9ulHryILK3UxU';
